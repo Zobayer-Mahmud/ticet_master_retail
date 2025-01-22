38f1d921c608e3059bd951252b02c29f669bbed3
Zobayer <zobayermahmud00@gmail.com>
2025-01-22 12:31:15 +0600

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("kotlin-parcelize")
+
+}
+
+android {
+    namespace = "com.example.ticket_master_retail_update"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.ticket_master_retail_update"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+//new dependency
+//
+    implementation (libs.secure.entry)
+//
+
+    //This version doesn't sync  com.github.romandanylyk:pageindicatorview:v.1.0.3
+//    implementation(libs.tickets)
+//// Accounts SDK
+    implementation(libs.authentication)
+//// Retail SDK
+    implementation(libs.purchase)
+    implementation(libs.prepurchase)
+    implementation(libs.discoveryapi)
+    implementation(libs.foundation)
+
+
+
+
+}
---
+++ app/src/test/java/com/example/ticket_master_retail_update/ExampleUnitTest.kt
@@ -0,0 +1,20 @@
+package com.example.ticket_master_retail_update
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
+
+//C1pkjWYZmFChCKF2NO0K0mOsD11pZHpA
+//Wyzrfriends25
